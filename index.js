996536f6f1561718a16d03f86b12c1d63769171c
Rafael Alves Florindo <rafaelflorindo@hotmail.com>
2025-05-07 22:12:02 -0300

    Add files via upload

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const sequelize = require("./database");
 const Produto = require("./models/Produto");
+const Usuario = require("./models/Usuario")
 
 const cors = require('cors');//habilita o Node a receber conexões
 require('dotenv').config();
@@ -9,7 +10,6 @@ const app = express();
 
 app.use(express.json());
 app.use(cors());
-
 
 const port = process.env.PORT || 3000;
 
@@ -43,16 +43,19 @@ app.get("/produtos/:id", async (req, res) => {
             return res.status(400).json({ error: "ID inválido. Use um número." });
         }
         const produto = await Produto.findByPk(id);
-        if (!produto) 
-            return res.status(404).json({ error: "Produto não encontrado" });
+        if (produto.length === 0) {
+            return res.status(200).json({ message: "Nenhum produto encontrado.", data: [] });
+        }
         res.status(200).json(produto);
     } catch (error) {
         res.status(500).json({ error: "Erro ao buscar o produto." });
     }
 });
 
-
 app.post("/produtos", async (req, res) => {
+
+    const {nome, quantidade, preco} = req.body;
+    
     if (typeof nome !== "string" || nome.trim() === "") {
         return res.status(400).json({ error: "Nome é obrigatório e deve ser uma string." });
     }
@@ -94,11 +97,11 @@ app.put("/produtos/:id", async (req, res) => {
         }
 
         const produto = await Produto.findByPk(id);
-        if (!produto) {
+        if (produto.length === 0) {
             return res.status(404).json({ error: "Produto não encontrado." });
         }
 
-        await produto.update({ nome, quantidade, preco });
+        await produto.update(req.body);
         res.status(200).json(produto);
     } catch (error) {
         console.error(error);
@@ -109,17 +112,13 @@ app.put("/produtos/:id", async (req, res) => {
 app.delete("/produtos/:id", async (req, res) => {
     try {
         const id = Number(req.params.id);
-
         if (isNaN(id)) {
             return res.status(400).json({ error: "ID inválido. Use um número." });
         }
-
         const produto = await Produto.findByPk(id);
-
-        if (!produto) {
+        if (produto.length === 0) {
             return res.status(404).json({ error: "Produto não encontrado." });
         }
-
         await produto.destroy();
         res.status(200).json({ message: "Produto removido com sucesso!" });
     } catch (error) {
@@ -128,6 +127,126 @@ app.delete("/produtos/:id", async (req, res) => {
     }
 });
 
+//************************************************ */
+app.get("/usuarios", async (req, res) => {
+    try {
+        const usuarios = await Usuario.findAll();
+        if (usuarios.length === 0) {
+            return res.status(200).json({ message: "Nenhum usuário encontrado.", data: [] });
+        }
+        res.status(200).json(usuarios);
+    } catch (error) {
+        console.error(error);
+        res.status(500).json({ error: "Erro ao buscar usuarios." });
+    }
+});
+
+app.get("/usuarios/:id", async (req, res) => {
+    try {
+        const id = Number(req.params.id);
+        if (isNaN(id)) {
+            return res.status(400).json({ error: "ID inválido. Use um número." });
+        }
+        const usuario = await Usuario.findByPk(id);
+        if (usuario.length === 0) {
+            return res.status(200).json({ message: "Nenhum usuário encontrado.", data: [] });
+        }
+        res.status(200).json(usuario);
+    } catch (error) {
+        res.status(500).json({ error: "Erro ao buscar o usuário." });
+    }
+});
+
+app.post("/usuarios", async (req, res) => {
+
+    const {nome, email, senha} = req.body;
+    
+    if (typeof nome !== "string" || nome.trim() === "") {
+        return res.status(400).json({ error: "Nome é obrigatório e deve ser uma string." });
+    }
+    if (typeof email !== "string" || email.trim() === "") {
+        return res.status(400).json({ error: "E-mail é obrigatório e deve ser uma string." });
+    }
+    if (typeof senha !== "string" || senha.trim() === "") {
+        return res.status(400).json({ error: "Senha é obrigatório e deve ser uma string." });
+    }
+   
+    try {
+        const novoUsuario = await Usuario.create(req.body);
+        res.status(201).json(novoUsuario);
+    } catch (error) {
+        res.status(500).json({ error: "Erro ao criar usuário." });
+    }
+});
+
+app.put("/usuarios/:id", async (req, res) => {
+    const {nome, email, senha} = req.body;
+    
+    if (typeof nome !== "string" || nome.trim() === "") {
+        return res.status(400).json({ error: "Nome é obrigatório e deve ser uma string." });
+    }
+    if (typeof email !== "string" || email.trim() === "") {
+        return res.status(400).json({ error: "E-mail é obrigatório e deve ser uma string." });
+    }
+    if (typeof senha !== "string" || senha.trim() === "") {
+        return res.status(400).json({ error: "Senha é obrigatório e deve ser uma string." });
+    }
+   
+
+    try {
+        const id = Number(req.params.id);
+        if (isNaN(id)) {
+            return res.status(400).json({ error: "ID inválido. Use um número." });
+        }
+
+        const usuario = await Usuario.findByPk(id);
+        if (usuario.length === 0) {
+            return res.status(404).json({ error: "Usuario não encontrado." });
+        }
+
+        await usuario.update(req.body);
+        res.status(200).json(usuario);
+    } catch (error) {
+        console.error(error);
+        res.status(500).json({ error: "Erro ao atualizar usuário." });
+    }
+});
+
+app.delete("/usuarios/:id", async (req, res) => {
+    try {
+        const id = Number(req.params.id);
+        if (isNaN(id)) {
+            return res.status(400).json({ error: "ID inválido. Use um número." });
+        }
+        const usuario = await Usuario.findByPk(id);
+        if (usuario.length === 0) {
+            return res.status(404).json({ error: "Usuario não encontrado." });
+        }
+        await usuario.destroy();
+        res.status(200).json({ message: "Usuario removido com sucesso!" });
+    } catch (error) {
+        console.error(error);
+        res.status(500).json({ error: "Erro ao remover usuario." });
+    }
+});
+/******************************************************* */
+app.post("/auth/login", async(req, res)=>{
+    const {email, senha} = req.body;
+    const usuario = await Usuario.findOne({where : {email}})
+    
+    if(!usuario)
+        return res.status(500).json({erro: "Usuário não encontrado"})
+   
+    if (senha === usuario.senha){
+        return res.status(200).json({
+            token: "123",
+            message:"Usuário Autenticado com Sucesso",
+            data: usuario
+        })
+    }else{
+        return res.status(500).json({message:"Senha invalida"})
+    }
+})
 app.listen(port, function(){
     console.log(`Servidor Rodando em: http://localhost:${port}`);
 })
---
+++ models/Usuario.js
@@ -0,0 +1,23 @@
+const { DataTypes } = require("sequelize");
+const sequelize = require("../database");
+
+const Usuario = sequelize.define("Usuario", {
+    nome: {
+        type: DataTypes.STRING,
+        allowNull: false,
+    },
+    email: {
+        type: DataTypes.STRING,
+        allowNull: false,
+        unique: true,
+        validate:{
+            isEmail:true
+        }
+    },
+    senha: {
+        type: DataTypes.STRING,
+        allowNull: false,
+    }
+});
+
+module.exports = Usuario;
